fac72e81708e601e1fb9d4c54f9e1445b1c136a1
Jhonacode <jhonacode2020@gmail.com>
2025-02-15 23:55:02 +1300

    Ok my bad, same model from rust struct, for now.

--- lib/src/model/local_db_request_model.dart
+++ lib/src/model/local_db_request_model.dart
@@ -1,38 +1,37 @@
 /// Hash is frm rust and las update is from dart fucntion internal.
 class LocalDbRequestModel{
   final String id;
-  final String? lastUpdate; /// Optional en el constructor por si el suuario decide poner por defecto uan fecha, de lo contrario la fucnion pone la actual o por si viene del servidor
-  final Map<String, dynamic> data; /// en general los datos son un  map asi sea solo un registro de un dato, este sera un map y asi evito la pelea de tiposd e datos.
+  final String? hash;
+  final Map<String, dynamic> data;
 
-  LocalDbRequestModel({required this.id, this.lastUpdate, required this.data});
-
+  LocalDbRequestModel({
+    required this.id,
+    this.hash,
+    required this.data,
+  });
 
   Map<String, dynamic> toJson() => {
     'id': id,
-    'last_update': lastUpdate,
-    'data': data
+    'hash': "${DateTime.now().millisecondsSinceEpoch}",
+    'data': data,
   };
 
   factory LocalDbRequestModel.fromJson(Map<String, dynamic> json) => LocalDbRequestModel(
     id: json['id'],
-    lastUpdate: json['last_update'],
-    data: json['data']
+    hash: json['hash'],
+    data: json['data'],
   );
-
 
   @override
   String toString() {
-    return 'LocalDbModel{id: $id, last_update: $lastUpdate, data: $data}';
+    return 'LocalDbModel{id: $id, hash: $hash, data: $data}';
   }
 
-
-  LocalDbRequestModel copyWith({String? id, String? lastUpdate, Map<String, dynamic>? data}){
-    return LocalDbRequestModel(
-      id: id ?? this.id,
-      lastUpdate: lastUpdate ?? this.lastUpdate,
-      data: data ?? this.data
-    );
-  }
+  LocalDbRequestModel copyWith({String? id, String? hash, Map<String, dynamic>? data}) => LocalDbRequestModel(
+    id: id ?? this.id,
+    hash: hash ?? this.hash,
+    data: data ?? this.data,
+  );
 
 }
 
